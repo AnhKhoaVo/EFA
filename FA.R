610ab3989465fb480faaf27b0b9b11302f5a74d6
Anh Khoa Vo <a.vo@alumni.ubc.ca>
2017-11-14 14:41:04 -0800

    Correlate the left out variables to LMS52

--- FA.R
+++ FA.R
@@ -26,7 +26,7 @@ factor_lm <- function(x) {
   return(list(f1,f2,f3,f4,f5))
 }
 
-factor_lm(p_week0)
+factor_lm(cfa2_week0)
 
 cors <- cor(subset(mtcars, select = c(-gear,-carb)))
 cors2 <- sapply(subset(mtcars, select = c(-gear,-carb)), cor(mtcars,use = "pairwise.complete.obs"), y=mtcars$mpg)
@@ -38,11 +38,12 @@ sapply(cfa2_week0, cor, y=cfa2_week0$Outcome)
 cfa2_week0 <- merge(cfa1_week0, p_week0, how = 'outer')
 cfa2_week0
 
+
+
 one <- subset(cfa2_week0, select = c(clusters))
 
 w <- cbind(one, p_week0)
 
-ab <- rbind.fill(cfa2_week0, p_week0)
 
 cfa2_week0 <- cbind(cfa1_week0, p_week0)
 cfa2_week0
@@ -50,3 +51,18 @@ cfa2_week0
 c <- cor(factors, use="pairwise.complete.obs")
 c <- as.data.frame(c)
 b <- c[1:3]
+
+
+LeftOut <- c("HGB00","HCT00","RBC00","STP00","ALB00","CAB00","CHO00",
+             "AST00","CK000","ALT00","BUN00","BC900","BUA00","P0400",
+             "MCV00","MCH00", "CHC00")
+
+
+
+LeftOutVars <- cfa2_week0[ , !(names(cfa2_week0) %in% LeftOut)]
+
+CorLeftOut <- as.data.frame(cor(LeftOutVars, use="pairwise.complete.obs"))
+CorLeftOut <- as.data.frame(CorLeftOut[1])
+
+
+cor.test(cfa2_week0$CLB00, cfa2_week0$TLAMS52, use="pairwise.complete.obs")
